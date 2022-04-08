9bdf78b1b1f8177cb1567cfb412c38228c48759b
Josh Murtha <murthajosh@gmail.com>
2022-04-07 20:26:54 -0500

    added switching to the headings for bio section.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const bioButtonShort = document.getElementById('bio-short')
 const bioButtonMedium = document.getElementById('bio-medium')
 const bioButtonLong = document.getElementById('bio-long')
 
+const shortBioHeading = document.getElementById('short-bio-heading')
+const mediumBioHeading = document.getElementById('medium-bio-heading')
+const longBioHeading = document.getElementById('long-bio-heading')
+
 const shortBio = document.getElementById('short-bio')
 const mediumBio = document.getElementById('medium-bio')
 const longBio = document.getElementById('long-bio')
@@ -10,14 +14,26 @@ if (document.querySelector('input[name="bio-length"]')) {
     document.querySelectorAll('input[name="bio-length"]').forEach((elem) => {
       elem.addEventListener("change", function(event) {
         if (bioButtonShort.checked) {
+            shortBioHeading.classList.remove('hidden')
+            mediumBioHeading.classList.add('hidden')
+            longBioHeading.classList.add('hidden')
+
             shortBio.classList.remove('hidden')
             mediumBio.classList.add('hidden')
             longBio.classList.add('hidden')
         } else if (bioButtonMedium.checked) {
+            shortBioHeading.classList.add('hidden')
+            mediumBioHeading.classList.remove('hidden')
+            longBioHeading.classList.add('hidden')
+
             shortBio.classList.add('hidden')
             mediumBio.classList.remove('hidden')
             longBio.classList.add('hidden')
         } else if (bioButtonLong.checked) {
+            shortBioHeading.classList.add('hidden')
+            mediumBioHeading.classList.add('hidden')
+            longBioHeading.classList.remove('hidden')
+
             shortBio.classList.add('hidden')
             mediumBio.classList.add('hidden')
             longBio.classList.remove('hidden')
