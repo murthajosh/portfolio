85dd780a3d9745f119b61021c1bfc67b6b3539b5
Josh Murtha <murthajosh@gmail.com>
2022-05-01 13:16:40 -0500

    got the responsive header in there. I'd still ike to make it animate in from the top like a falling curtain, but the basics are there now and they're working.

--- base/components/header/header.js
+++ base/components/header/header.js
@@ -1,1 +1,11 @@
-const menuButton = document.getElementById('icon-hamburger')
+const navCurtain = document.querySelector('.nav-curtain')
+const pageContent = document.getElementById('page-content')
+
+document.addEventListener('click', function(e) {
+    if (!e.target.matches('.nav-toggle')) {
+        return
+    } else {
+        navCurtain.classList.toggle('hidden')
+        pageContent.classList.toggle('disabled')
+    }
+})
