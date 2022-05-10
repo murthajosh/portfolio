6afee1e6ed68ea3e207901c90e3a8f93f82e1311
Josh Murtha <murthajosh@gmail.com>
2022-05-10 10:35:18 -0500

    fixed JS for that email copy button.

--- public/pages/contact/contact.js
+++ public/pages/contact/contact.js
@@ -1,6 +1,7 @@
 const emailAddress = document.querySelector('#my-email')
+const copyButton = document.querySelector('.copy-button')
 
 
-emailAddress.addEventListener('click', () => {
+copyButton.addEventListener('click', () => {
     navigator.clipboard.writeText(emailAddress.innerHTML);
 })
