c70a40bf3d5c7caffd26af0a15668a3745fa64fb
Josh Murtha <murthajosh@gmail.com>
2022-06-26 10:00:13 -0500

    success message on contact page. removed bootstrappy card shadows on project grid.

--- public/pages/contact/contact.js
+++ public/pages/contact/contact.js
@@ -1,7 +1,8 @@
-const emailAddress = document.querySelector('#my-email')
 const copyButton = document.querySelector('.copy-button')
+const successMessage = document.querySelector('.success-message')
 
 
 copyButton.addEventListener('click', () => {
     navigator.clipboard.writeText('murthajosh@gmail.com');
+    successMessage.classList.remove('hidden');
 })
