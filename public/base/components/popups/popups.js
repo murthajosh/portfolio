dd8e91e8c2383ab810b360332102038910113a46
Josh Murtha <murthajosh@gmail.com>
2022-06-26 17:50:33 -0500

    changed the popups from pure css to js. much smoother ux now.

--- public/base/components/popups/popups.js
+++ public/base/components/popups/popups.js
@@ -1,11 +1,20 @@
 const triggers = document.querySelectorAll('.collection-grid-item')
+const overlays = document.querySelectorAll('.overlay')
 
 triggers.forEach(trigger => {
     trigger.addEventListener('click', () => {
         let target=trigger.getAttribute('name')
         console.log(target)
 
-        let overlay=document.getElementById(target)
-        overlay.classList.remove('hidden')
+        let projectPopup=document.getElementById(target)
+        projectPopup.classList.remove('hidden')
+    })
+})
+
+overlays.forEach(overlay => {
+    overlay.addEventListener('click', (e) => {
+        if (e.target.classList.contains('overlay') || e.target.classList.contains('close')) {
+            overlay.classList.add('hidden')
+        }
     })
 })
